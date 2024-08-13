50291de68d7146304decf95b0ca3015c8de85c63
KevinGil12C <kebo.jcg77@gmail.com>
2024-08-12 19:58:13 -0600

    update

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.hazmat.primitives.serialization import load_pem_public_key
 from cryptography.hazmat.primitives.serialization import BestAvailableEncryption
 from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.hazmat.primitives.serialization import PublicFormat, PrivateFormat
@@ -15,7 +12,6 @@ import cryptography.x509 as x509
 def generate_certificate():
     # Solicitar información al usuario
     nombre = input("Ingrese su nombre: ")
-    edad = input("Ingrese su edad: ")
     apodo = input("Ingrese su apodo: ")
     nombre_favorito = input("Ingrese su nombre favorito: ")
     password = input("Ingrese una contraseña para la clave privada: ").encode()
@@ -33,17 +29,18 @@ def generate_certificate():
     subject = issuer = x509.Name([
         x509.NameAttribute(NameOID.COMMON_NAME, nombre),
         x509.NameAttribute(NameOID.GIVEN_NAME, apodo),
-        x509.NameAttribute(NameOID.SURNAME, nombre_favorito),
-        x509.NameAttribute(NameOID.UNIQUE_IDENTIFIER, edad),
+        x509.NameAttribute(NameOID.SURNAME, nombre_favorito)
     ])
 
+    now = datetime.datetime.now(datetime.timezone.utc)
+    
     cert_builder = x509.CertificateBuilder(
         issuer_name=issuer,
         subject_name=subject,
         public_key=public_key,
         serial_number=x509.random_serial_number(),
-        not_valid_before=datetime.datetime.utcnow(),
-        not_valid_after=datetime.datetime.utcnow() + datetime.timedelta(days=365)
+        not_valid_before=now,
+        not_valid_after=now + datetime.timedelta(days=365)
     )
 
     # Firmar el certificado con la clave privada
